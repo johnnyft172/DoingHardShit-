5b3df846ccc5371a6c92d0007cb7968b6747203c
jmintegr1 <jmintegr1@gmail.com>
2018-12-30 21:49:27 -0500

    First Thing First

---
+++ src/FirstThingFirst/DoTheHardestThingFirst.java
@@ -0,0 +1,7 @@
+package FirstThingFirst;
+
+public class DoTheHardestThingFirst {
+    public static void main(String[] args) {
+        System.out.println("I know the path to daily freedom is doing the scariest and hardest thing first with heart, faith, & intensity");
+    }
+}
