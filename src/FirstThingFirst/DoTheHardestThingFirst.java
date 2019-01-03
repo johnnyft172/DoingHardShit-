770248e0cde76669406d36aabf6c7951327ce772
jmintegr1 <jmintegr1@gmail.com>
2019-01-02 22:38:40 -0500

    Hard Shit Always

--- src/FirstThingFirst/DoTheHardestThingFirst.java
+++ src/FirstThingFirst/DoTheHardestThingFirst.java
@@ -2,6 +2,7 @@ package FirstThingFirst;
 
 public class DoTheHardestThingFirst {
     public static void main(String[] args) {
-        System.out.println("I know the path to daily freedom is doing the scariest and hardest thing first with heart, faith, & intensity");
+        System.out.println("I know the path to daily freedom is doing the scariest and hardest thing first with heart, faith, & intensity. So " +
+                "FIRST THING FIRST");
     }
 }
